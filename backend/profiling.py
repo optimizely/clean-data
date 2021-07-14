b3d79bab27e5e7c959360af87485cd37987325d7
German Ramirez <glramirez@Germans-MacBook-Pro.local>
2021-07-14 09:53:12 -0500

    Update to calculate the customers with empty cells in the report

--- backend/profiling.py
+++ backend/profiling.py
@@ -1,3 +1,4 @@
+from os import error
 import pandas as pd
 import json
 import numpy as np
@@ -28,9 +29,20 @@ def generate_data(df):
         description_profile['variables'][key].pop('value_counts_without_nan', None)
         description_profile['variables'][key].pop('value_counts_index_sorted', None)
 
+    column_list = ['epi_universal_id','parent_id','parent_name','skus','target_account','running_experiments','duns_id','naics','sic','region','territory','target_account']
+    df_nullvalues = df.drop(columns=column_list, axis=1, errors='ignore')
+    withnulls = df_nullvalues.isna().any(axis=1).sum()
+
+    nulls = {
+        "without_nulls": df.shape[0] - withnulls,
+        "with_nulls": withnulls,
+        "perc_nulls": round(withnulls/df.shape[0], 5)
+    }
+
     variable_profile = {"variables" : description_profile["variables"]}
     overall_profile = {"table" : description_profile["table"]}
     overall_profile.update(variable_profile)
+    overall_profile["table"].update(nulls)
 
     json_data = json.dumps(overall_profile, cls=NpEncoder, ensure_ascii=False)
     return json_data
