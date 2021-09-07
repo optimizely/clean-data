330508dc6d454afc4fdff0ed49bef811c3843dcd
German Ramirez <glramirez@Germans-MacBook-Pro.local>
2021-09-07 16:41:51 -0500

    update the project to use Axion lib and redo the front

--- frontend/src/components/ColumnsView.js
+++ frontend/src/components/ColumnsView.js
@@ -1,41 +1,150 @@
-import { Fragment } from "react";
-import ColumnDetail from './ColumnDetail';
-import styled from "styled-components";
+import React from "react";
+import SolutionButton from './SolutionButton';
+import { Table } from "optimizely-oui"
+import PropTypes from 'prop-types';
 
-const Title = styled.div`
-    font-family: 'Roboto';
-    font-size: 2.5vw;
-    font-weight: 600;
-    padding: 10px;
-`
+class SortedTable extends React.Component {
+    state = {
+        tableContents: this.props.tableContents,
+        currentOrder: this.props.currentOrder,
+        currentSortedColumn: this.props.currentSortedColumn,
+    };
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 10px 5px;
-    width: 100%;
-    height: 90vh;
-    overflow-y: auto;
-`
+    handleSorting = columnName => {
+        // If the user isn't switching sort columns, toggle the sort direction
+        const sortToggleMap = {
+            ["asc"]: "desc",
+            ["desc"]: "asc",
+        };
+        let newOrder = "asc";
+        if (this.state.currentSortedColumn === columnName) {
+            newOrder = sortToggleMap[this.state.currentOrder];
+        }
+        this.setState({ currentOrder: newOrder, currentSortedColumn: columnName });
+        if (newOrder === "asc") {
+            this.state.tableContents.sort((a, b) => (a[columnName] > b[columnName] ? 1 : -1));
+        } else {
+            this.state.tableContents.sort((b, a) => (a[columnName] > b[columnName] ? 1 : -1));
+        }
+    };
 
+    componentDidUpdate(prevProps, prevState) {
+        if (!prevProps.tableContents.every((value, index) => value === this.props.tableContents[index])) {
+          this.setState({tableContents : this.props.tableContents})
+        }
+      }
 
-function ColumnsView(props) {
-
-
-    return (
-        <Fragment>
-            <Title>Column Validation</Title>
-            <Container>
-                    {props.data.map((column) => (
-                        <ColumnDetail key = {column[0]} 
-                                name = {column[0]} 
-                                detail = {column[1]} 
-                                buttonName = {props.buttonName} 
-                                tableLabel = {props.tableLabel}/>
-                    ))}
-            </Container>
-        </Fragment>
-    )
+    render() {
+        const { currentOrder, tableContents } = this.state;
+        return (
+            <Table>
+                <Table.THead>
+                    <Table.TR>
+                        <Table.TH
+                            sorting={{
+                                canSort: true,
+                                handleSort: () => this.handleSorting("Column Name"),
+                                order: currentOrder,
+                            }}
+                        >
+                            Column Name
+                        </Table.TH>
+                        <Table.TH
+                            sorting={{
+                                canSort: true,
+                                handleSort: () => this.handleSorting("Missing Rows"),
+                                order: currentOrder,
+                            }}
+                        >
+                            Missing Rows
+                        </Table.TH>
+                        <Table.TH
+                            sorting={{
+                                canSort: true,
+                                handleSort: () => this.handleSorting("Missing Percentage"),
+                                order: currentOrder,
+                            }}
+                        >
+                            Missing Percentage
+                        </Table.TH>
+                        <Table.TH>
+                            Solution/Status 
+                        </Table.TH>
+                    </Table.TR>
+                </Table.THead>
+                <Table.TBody>
+                    {tableContents.map((row, idx) => {
+                        return (
+                            <Table.TR key={idx}>
+                                <Table.TD>{row["Column Name"]}</Table.TD>
+                                <Table.TD className={row["className"]} width="20%">{row["Missing Rows"]} </Table.TD>
+                                <Table.TD className={row["className"]}> {row["Missing Percentage"]} </Table.TD>
+                                <Table.TD> {row["Solution/Status"]} </Table.TD>
+                            </Table.TR>
+                        );
+                    })}
+                </Table.TBody>
+            </Table>
+        );
+    }
 }
+
+SortedTable.propTypes = {
+    currentOrder: PropTypes.string,
+    currentSortedColumn: PropTypes.string,
+    tableContents: PropTypes.arrayOf(
+        PropTypes.shape({
+            "Column Name": PropTypes.string,
+            "Missing Rows": PropTypes.number,
+            "Missing Percentage": PropTypes.string,
+            "Solution/Status": PropTypes.node,
+            "className": PropTypes.string,
+        })
+    ),
+};
+
+function ColumnsView (props) {
+    const currentOrder = "asc";
+    const currentSortedColumn = "Missing Rows";
+    const tableLabel = props.tableLabel;
+
+    const tableContents = Object.keys(props.data).reduce((acc, value) => {
+          acc.push([value, props.data[value]]);
+          return  acc
+    },[]).map(
+        (column) => {
+            let columnName = column[0];
+            let columnDetails = column[1];
+
+            let solutionColumn = <SolutionButton
+                                        name={columnName}
+                                        tableScope={props.buttonName}
+                                        p_missing={columnDetails.p_missing}
+                                        tableLabel={tableLabel}
+                                />;
+            return (
+                {
+                    "Column Name": columnName,
+                    "Missing Rows": columnDetails.n_missing,
+                    "Missing Percentage": (columnDetails.p_missing * 100).toFixed(1) + "%",
+                    "Solution/Status": solutionColumn,
+                    "className": (columnDetails.p_missing > 0.2 ? "color--bad-news" : "color--good-news")
+                }
+            )
+        }
+    );
+        console.log({tableContents})
+        // See top of file for implementation of a sortable table
+        return (
+            <div>
+                <SortedTable
+                    tableContents={tableContents}
+                    currentOrder={currentOrder}
+                    currentSortedColumn={currentSortedColumn}
+                />
+            </div>
+        );
+
+};
 
 export default ColumnsView;
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,53 +1,34 @@
-import styled from "styled-components";
 
-const Container = styled.div`
-    display: flex;
-    flex-grow: 1;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    width: 40vw;
-    hight: 100%;
-    padding: 10px 10px;
-`
+import { ListGroup, Button } from "optimizely-oui";
 
-const TableButton = styled.button`
-    width: 100%;
-    hight: auto;
-    font-family: Helvetica;
-    font-size: 1.3vw;
-    padding: 10px 0;
-    margin: 5px 0;
-    border-radius: 10px;
-    background-color: azure;
-    :hover {background-color: #29B6F6}
-`
-const Label = styled.div`
-    font-family: Helvetica;
-    font-size: 1.2vw;
-    font-weight: 600;
-    color: white;
-    hight: 12vh;
-    width: 100%;
-    padding: 10px 0;
-    text-align: center;
-`
 const Sidebar = (props) => {
     return (
-        <Container>
-            <Label style={{backgroundColor:"#3722f6"}}>UFDM - Blue Layer</Label>
-            {props.blue.map((table) => {
-                if (table.substring(0, 9) !== 'snapshot_') {
-                    return (
-                        <TableButton key={table}
-                            onClick={() => props.getTableReports('ufdm_blue', table)}
-                        >
-                        {table}
-                        </TableButton>
-                    )
-                }
-            })}
-        </Container>
+        <>
+            <div className="flex flex--column width--1-2">
+                <ListGroup 
+                    title="UFDM - Blue Layer" 
+                    subtitle="Agregation tables from where you can create Report tables in Green Layer" 
+                    className="width--1-1 color--brand">
+                    {props.blue.map((table) => {
+                        return (
+                            <ListGroup.Item 
+                                key={table}
+                                density="tight"
+                            >
+                                <Button 
+                                    key={table}
+                                    style="plain"
+                                    width="default"
+                                    onClick={() => props.getTableReports('ufdm_blue', table)}
+                                >
+                                        {table}
+                                </Button>
+                            </ListGroup.Item>
+                        )
+                    })}
+                </ListGroup>
+            </div>
+        </>
     )
 }
 
